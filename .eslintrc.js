21979bd6909e352e81c7c0a7031ae1278fa2df13
John Osborn <john.r.osborn@gmail.com>
2023-04-20 11:21:57 +1000

    new file:   .eslintignore 	new file:   .eslintrc.js 	new file:   .gitignore 	new file:   assets/icon.png 	new file:   assets/timeline.png 	new file:   assets/timeline1.png 	new file:   capabilities.json 	new file:   dist/dateSel4A1A0033E6F54D1B809B6E51058D54E3.23.03.24.pbiviz 	new file:   dist/dateSel4A1A0033E6F54D1B809B6E51058D54E3.23.04.03.pbiviz 	new file:   dist/package.json 	new file:   package-lock.json 	new file:   package.json 	new file:   pbiviz.json 	new file:   src/constants.tsx 	new file:   src/dateRangeSelector.tsx 	new file:   src/datefield.tsx 	new file:   src/dateinput.tsx 	new file:   src/dateintervalpicker.tsx 	new file:   src/datemove.tsx 	new file:   src/datemovekeys.tsx 	new file:   src/daterange.tsx 	new file:   src/daterangecard.tsx 	new file:   src/daterangetoprow.tsx 	new file:   src/dateutils.tsx 	new file:   src/dualslider.tsx 	new file:   src/helpprovider.tsx 	new file:   src/initsettings.ts 	new file:   src/initstate.tsx 	new file:   src/interface.tsx 	new file:   src/rangeslider.tsx 	new file:   src/rngetooltip.tsx 	new file:   src/settings.ts 	new file:   src/sliderstyles.tsx 	new file:   src/stepsmenu.tsx 	new file:   src/steptoggle.tsx 	new file:   src/timeline.tsx 	new file:   src/togglesliderbutton.tsx 	new file:   src/usecurrent.tsx 	new file:   src/visual.ts 	new file:   tsconfig.json 	new file:   tslint.json 	new file:   webpack.statistics.dev.html 	new file:   webpack.statistics.prod.html

---
+++ .eslintrc.js
@@ -0,0 +1,18 @@
+module.exports = {
+  env: {
+    browser: true,
+    es6: true,
+    es2017: true,
+  },
+  root: true,
+  parser: "@typescript-eslint/parser",
+  parserOptions: {
+    project: "tsconfig.json",
+    tsconfigRootDir: ".",
+  },
+  plugins: ["powerbi-visuals"],
+  extends: ["plugin:powerbi-visuals/recommended"],
+  rules: {
+    // "import/no-extraneous-dependencies": ["error", { devDependencies: true }],
+  },
+};
